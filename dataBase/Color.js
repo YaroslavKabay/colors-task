0eeb9e9f5dfd009082b91353d98ac4027b26d477
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-12-13 00:04:14 +0200

    back end demo

--- dataBase/Color.js
+++ dataBase/Color.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('mongoose');
 
 const colorSchema = new Schema({
     color: { type: String, trim: true, required: true, unique: true },
-
 }, {
     timestamps: true,
     versionKey: false
